5222a948f0a9cf42fccfb212dfc4fb543a7dae6c
chim7a <chimalawrence11@gmail.com>
2024-11-07 11:02:48 +0100

    update

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 import mongoose from "mongoose";
+import "dotenv/config";
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_KEY);
+  } catch (error) {
+    console.log(err);
+    console.log("An error occured");
+  }
+}
