2b163e1d5a63ffe65e870f136623d80925c4c4ae
chim7a <chimalawrence11@gmail.com>
2024-11-07 11:42:03 +0100

    connecting to db and creating DB

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+const User = new mongoose.model("Users", userSchema);
+
+async function addUser(params) {
+  const createdUser = await User.create({ name: "Chima", age: 52 });
+  console.log(createdUser);
+}
+
+// Connecting to Mongo database.
 async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_KEY);
     console.log("Database connection successful");
+    addUser();
   } catch (error) {
     console.log(err);
     console.log("An error occured");
   }
 }
+
+connectDB();
